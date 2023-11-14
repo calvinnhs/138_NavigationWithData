2690157163868599ccf125daf65d7518dffa50be
Calvin Hendrawan <tugasakun22@gmail.com>
2023-11-14 21:26:03 +0700

    menambahkan spacer dan button di halamanfrom

--- app/src/main/java/com/example/navdata/HalamanForm.kt
+++ app/src/main/java/com/example/navdata/HalamanForm.kt
@@ -2,7 +2,10 @@ package com.example.navdata
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,5 +44,10 @@ fun HalamanFrom(
         OutlinedTextField(value = alamatTxt, onValueChange = { alamatTxt = it }, label = {
             Text(text = stringResource(id = R.string.alamatp))
         })
+        
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(onClick = {onSubmitButtonClicked(listData)}) {
+            Text(text = stringResource(id = R.string.confirm))
+        }
     }
 }
